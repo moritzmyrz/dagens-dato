8485d9945154bcf49cb90d960da7004b7478e6e2
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-17 18:57:31 +0100

    IgnoreCase Fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "style/App.scss";
+import "style/app.scss";
 import { Dato } from "../functions/Dato";
 import Main from "./Main";
 import Title from "./Title";
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Clock from "react-live-clock";
-import "style/Main.scss";
+import "style/main.scss";
 import { GetMonth } from "../functions/GetMonth";
 import { GetWeekNum } from "../functions/GetWeekNum";
 
