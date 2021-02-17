47ff6e55d60022bc9db418ac384219a528c4be6f
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-17 10:37:24 +0100

    import fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import "style/App.scss";
+import "./style/App.scss";
 
 ReactDOM.render(
   <React.StrictMode>
