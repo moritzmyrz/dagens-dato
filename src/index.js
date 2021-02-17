4ae8e2a070dd534c95cf44ac6679fc6286fa77db
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-17 10:33:57 +0100

    fix import scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/App.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import "style/App.scss";
 
 ReactDOM.render(
   <React.StrictMode>
