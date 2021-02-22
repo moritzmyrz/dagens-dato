a9a9d5ae5ddfc4dcc78f7197f0d1e0cd5cf84dce
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-22 10:28:14 +0100

    Day Description

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -7,44 +7,45 @@ const cors = "https://secret-ocean-49799.herokuapp.com/";
 const api = "https://secret-shore-24919.herokuapp.com/data/";
 
 type AppProps = {
-  time: Date;
+	time: Date;
 };
 
 const Events: React.FC<AppProps> = ({ time }: AppProps) => {
-  const [events, setEvents] = useState([]);
+	const [events, setEvents] = useState({ data: [], description: "" });
 
-  useEffect(() => {
-    axios
-      .get(cors + api, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        setEvents(response.data);
-      });
-  }, []);
-  // eslint-disable-next-line
-  const items: any = [];
+	useEffect(() => {
+		axios
+			.get(cors + api, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+			.then((response) => {
+				console.log(response);
+				setEvents(response.data);
+			});
+	}, []);
+	// eslint-disable-next-line
+	const items: any = [];
 
-  let i = 0;
+	let i = 0;
 
-  events.forEach((str) => {
-    items.push(i % 2 == 0 ? <h2 key={str}>{str}</h2> : <p key={str}>{str}</p>);
-    i++;
-  });
+	events.data.forEach((str) => {
+		items.push(i % 2 == 0 ? <h2 key={str}>{str}</h2> : <p key={str}>{str}</p>);
+		i++;
+	});
 
-  return (
-    <div className="events-main">
-      <h1>I Dag, Den {`${time.getDate()}. ${GetMonth(time.getMonth())}`}</h1>
-      <div id="event-contents">
-        <h1>År</h1>
-        <h1></h1>
-        {items}
-      </div>
-    </div>
-  );
+	return (
+		<div className="events-main">
+			<h1>I Dag, Den {`${time.getDate()}. ${GetMonth(time.getMonth())}`}</h1>
+			<p>{events.description}</p>
+			<div id="event-contents">
+				<h1>År</h1>
+				<h1></h1>
+				{items}
+			</div>
+		</div>
+	);
 };
 
 export default Events;
