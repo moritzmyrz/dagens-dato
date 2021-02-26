fa65cc73a3107fca773cd192c4c5daabdc62e13f
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-26 23:11:10 +0100

    Theme Fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
-import { Button, ButtonGroup } from "@material-ui/core";
+import { Button, ButtonGroup, ThemeProvider } from "@material-ui/core";
 import { Dato } from "functions/Date";
+import { theme } from "functions/Theme";
 import * as React from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import "style/App.scss";
@@ -11,34 +12,36 @@ const App: React.FC = () => {
 	const day = 86400000;
 
 	return (
-		<div id="app-main">
-			<ButtonGroup>
-				<Button
-					variant="contained"
-					className="nav-btn"
-					id="navbtn1"
-					onClick={() => {
-						setTime(new Date(time.getTime() - 1 * day));
-					}}
-				>
-					<MdChevronLeft />
-				</Button>
-				<Button
-					variant="contained"
-					className="nav-btn"
-					id="navbtn2"
-					onClick={() => {
-						setTime(new Date(time.getTime() + 1 * day));
-					}}
-				>
-					<MdChevronRight />
-				</Button>
-			</ButtonGroup>
-			<div className="content">
-				<Main time={time}></Main>
-				<Title time={time}></Title>
+		<ThemeProvider theme={theme}>
+			<div id="app-main">
+				<ButtonGroup>
+					<Button
+						variant="contained"
+						className="nav-btn"
+						id="navbtn1"
+						onClick={() => {
+							setTime(new Date(time.getTime() - 1 * day));
+						}}
+					>
+						<MdChevronLeft />
+					</Button>
+					<Button
+						variant="contained"
+						className="nav-btn"
+						id="navbtn2"
+						onClick={() => {
+							setTime(new Date(time.getTime() + 1 * day));
+						}}
+					>
+						<MdChevronRight />
+					</Button>
+				</ButtonGroup>
+				<div className="content">
+					<Main time={time}></Main>
+					<Title time={time}></Title>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 };
 
