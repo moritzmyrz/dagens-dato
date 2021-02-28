385ffee35f9a9afc2fb20217b0c264607eb03e34
Moritz Myrseth <moritzmyrz@gmail.com>
2021-02-28 21:55:06 +0100

    Date Selector

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,48 @@
+import DateFnsUtils from "@date-io/date-fns";
 import { Button, ButtonGroup, ThemeProvider, Tooltip } from "@material-ui/core";
 import Zoom from "@material-ui/core/Zoom";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { Dato } from "functions/Date";
 import { GetMonth } from "functions/GetMonth";
 import { theme } from "functions/Theme";
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { CgCalendarToday } from "react-icons/cg";
 import { MdChevronLeft, MdChevronRight, MdHome } from "react-icons/md";
 import "style/App.scss";
 import Content from "./Content";
 
 const App: React.FC = () => {
-	const [time, setTime] = React.useState(Dato);
+	const [time, setTime] = useState(Dato);
 	const day = 86400000;
+	const [selectDateDialog, setselectDateDialog] = useState(false);
 
 	useEffect(() => {
 		document.title = `${time.getDate()}. ${GetMonth(time.getMonth())}`;
 	}, [time]);
 
+	const handleSetDateChange = (date) => {
+		setTime(date);
+		setselectDateDialog(false);
+	};
+
 	return (
 		<ThemeProvider theme={theme}>
+			<MuiPickersUtilsProvider utils={DateFnsUtils}>
+				<DatePicker
+					disableToolbar
+					className="hidden"
+					format="dd/MM"
+					open={selectDateDialog}
+					onClose={() => {
+						setselectDateDialog(false);
+					}}
+					value={time}
+					cancelLabel="Avbryt"
+					okLabel="OK"
+					onChange={handleSetDateChange}
+				/>
+			</MuiPickersUtilsProvider>
 			<div id="app-main">
 				<ButtonGroup>
 					<Tooltip title="Tilbake" arrow TransitionComponent={Zoom}>
@@ -45,11 +69,23 @@ const App: React.FC = () => {
 							<MdHome />
 						</Button>
 					</Tooltip>
+					<Tooltip title="Velg" arrow TransitionComponent={Zoom}>
+						<Button
+							variant="contained"
+							className="nav-btn"
+							id="navbtn2"
+							onClick={() => {
+								setselectDateDialog(true);
+							}}
+						>
+							<CgCalendarToday />
+						</Button>
+					</Tooltip>
 					<Tooltip title="Neste" arrow TransitionComponent={Zoom}>
 						<Button
 							variant="contained"
 							className="nav-btn"
-							id="navbtn3"
+							id="navbtn4"
 							onClick={() => {
 								setTime(new Date(time.getTime() + 1 * day));
 							}}
